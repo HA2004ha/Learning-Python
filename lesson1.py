680746e872a2ba622f7ec044f7927811801a439a
H.A <haskari129@gmail.com>
2025-02-28 11:27:36 +0330

    lesson 1 - 3 esfand 1403

---
+++ lesson1.py
@@ -0,0 +1,82 @@
+# x = 5
+# age = 10 # عدد صحیح
+
+# pi =  3.14
+
+# name = "Hossein"
+
+
+# numbers = [1, 2 ,3 , 4]
+# lst = [1, 3.14, "hello"]
+
+
+# student = {"name": "Hossein", "Age": 22,"game" :"..."} # { key: value}
+# name = "hossein"
+# Age = 22
+
+# correct = True 
+# wroung = False
+
+
+# is_correct = True 
+
+
+
+# print("hello world")
+# print("my age is 22")
+
+# name = input()
+
+# print("hello world")
+# print(name)
+
+
+# age = 5
+# name = input()
+
+
+
+# print(name)
+# print(age)
+
+
+
+# age = 20
+# name = "ali"
+# print( type(name) )
+
+# age = int(input())
+# name = int(input())
+# print(type(name))
+
+# age = int(input("what is your age:"))
+# name = input("what is your name:")
+# hobby = ""
+# game = = ""
+# last_name = ""
+
+
+# hossein is 20 years old
+# print(fr"{name} is {age} years old")
+
+# person = {"age":20,"name":"Hossein","last_name":"Askari"}
+# person2 = {"age":25,"name":"Ali","last_name":"sohraby"}
+
+# print(person["last_name"])
+# print(person2["name"])
+
+# game = "mario" "minecraft" "sonic"
+# game = ["mario","mincraft","sonic","something"]
+# game1 = "mario"
+# game2 = "mincraft"
+# game3 = "sonic"
+
+# print(game[2])
+
+
+# html css javascript طراح وبسایت
+
+# تمرین
+#  گرفتن نام و نام خانوادکی و شهر و سن 
+# ذخیره در یک دیکشنری و خروجی در ترمینال در یک خط
+# گرفتن چند سرگرمی و ذخیره در یک لیست و نشان دادن ان در ترمینال
